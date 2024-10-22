daa7ffcede2a0f1626257aa251eaa017025392f1
WaterRun <linzhangrun@outlook.com>
2024-10-22 11:35:40 +0800

    upload pypi

--- source/setup.py
+++ source/setup.py
@@ -1,5 +1,4 @@
-
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='simpsave',
@@ -7,7 +6,15 @@ setup(
     author='WaterRun',
     author_email='linzhangrun49@gmail.com',
     url='https://github.com/Water-Run/SimpSave',
-    description='',
-    packages=['simpsave'],
+    description='A lightweight Python library for simple persistent storage using .ini files.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),  # Automatically find the `ss` package
     install_requires=[],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
 )
